cffc76c630c75145009f78641aa0188511be63b4
RobMoore92 <s216147@students.cdu.edu.au>
2018-04-30 17:47:34 +0930

    updated layoutabit / adding tool functions

--- resources/js/project_main.js
+++ resources/js/project_main.js
@@ -4,7 +4,10 @@ function loadProject(){
     var title = document.getElementById('title1');
     var due = document.getElementById('due_date1');
     title.innerHTML = (new_title);
-    due.innerHTML = ('Due: '+ new_due)
-    
-    ;
+    due.innerHTML = ('Due: '+ new_due);
+}
+
+function save(){
+    var textbox = document.getElementById('textbox');
+    localStorage.setItem('save', textbox.textContent);
 }
